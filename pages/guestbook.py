15e2407e5217c794052c1cb34378ebc3e2629d50
JaeUng Lee <60501045+ww5702@users.noreply.github.com>
2025-03-18 15:36:28 +0900

    Update guestbook.py

--- pages/guestbook.py
+++ pages/guestbook.py
@@ -20,6 +20,11 @@ conn = baseSource.init()
 conn = baseSource.connect()
 cursor = conn.cursor()
 
+cursor.execute("DELETE FROM boards")  # 방명록 데이터 삭제
+cursor.execute("DELETE FROM like_records")  # 좋아요 기록 삭제
+conn.commit()
+st.experimental_rerun()
+
 ### 스타일 
 
 def local_css():
