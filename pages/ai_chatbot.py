c3a92cda7ff88afa5fa01e7bd36926c2f598a728
JaeUng Lee <60501045+ww5702@users.noreply.github.com>
2025-04-02 20:17:45 +0900

    Update ai_chatbot.py
    feat : sqlite3 관련 오류 해결

--- pages/ai_chatbot.py
+++ pages/ai_chatbot.py
@@ -3,6 +3,9 @@ import os
 import streamlit as st
 import sys
 import sqlite3
+import pysqlite3
+# sqlite3 관련 오류 해결
+sys.modules["sqlite3"] = pysqlite3
 import time
 
 from langchain_community.retrievers import TavilySearchAPIRetriever
@@ -17,7 +20,7 @@ import requests
 from bs4 import BeautifulSoup
 import fitz  # PyMuPDF
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain.schema import Document
 from langchain.prompts import PromptTemplate
 
@@ -28,7 +31,6 @@ st.set_page_config(
     layout="centered",  # "wide"에서 "centered"로 변경
     initial_sidebar_state="expanded"
 )
-
 
 # 현재 파일(ai_chatbot.py)의 위치를 기반으로 lawChatBot 경로 추가
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # lawChatBot 디렉토리 경로
