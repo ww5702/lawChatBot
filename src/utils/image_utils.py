0bbfb30843ee604a6c6d32113275b3f93c5adb9c
alswnsp411 <alswnsp411@naver.com>
2025-04-08 07:34:47 +0900

    refactor: image_utils 통합

--- src/utils/image_utils.py
+++ src/utils/image_utils.py
@@ -1,15 +1,7 @@
 import base64
 
 def get_image_as_base64(file_path):
-    """
-    이미지 파일을 base64 문자열로 변환합니다.
-    
-    Args:
-        file_path (str): 이미지 파일의 경로
-        
-    Returns:
-        str: base64로 인코딩된 이미지 문자열
-    """
+    """이미지를 base64로 인코딩하는 함수"""
     try:
         with open(file_path, "rb") as img_file:
             return base64.b64encode(img_file.read()).decode()
--- src/utils/main_image_utils.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-def get_image_as_base64(file_path):
-    """이미지를 base64로 인코딩하는 함수"""
-    try:
-        with open(file_path, "rb") as img_file:
-            return base64.b64encode(img_file.read()).decode()
-    except FileNotFoundError:
-        return None
